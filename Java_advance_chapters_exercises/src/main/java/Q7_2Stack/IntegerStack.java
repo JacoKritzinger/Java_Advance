904f1aa7f0e80c0ab67a75f9dafc2772bed803c3
jaco Kritzinger <jacokritzinger05@gmail.com>
2021-08-27 10:36:21 +0200

    27/08/21

---
+++ Java_advance_chapters_exercises/src/main/java/Q7_2Stack/IntegerStack.java
@@ -0,0 +1,11 @@
+package Q7_2Stack;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class IntegerStack {
+
+    private Deque<Integer> data = new ArrayDeque<Integer>();
+
+    
+}

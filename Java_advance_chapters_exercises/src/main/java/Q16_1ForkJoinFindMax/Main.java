195357e1769b3957b9d11cba965ecf637f94312a
jaco Kritzinger <jacokritzinger05@gmail.com>
2021-09-08 16:27:25 +0200

    08/09/2021

--- Java_advance_chapters_exercises/src/main/java/Q16_1ForkJoinFindMax/Main.java
+++ Java_advance_chapters_exercises/src/main/java/Q16_1ForkJoinFindMax/Main.java
@@ -14,15 +14,19 @@ public class Main {
             data[i] = ThreadLocalRandom.current().nextInt();
         }
 
-//        int max = Integer.MIN_VALUE;
-//        for (int value : data) {
-//            if (value > max) {
-//                max = value;
-//            }
-//        }
-//        System.out.println("Max value found:" + max);
-        
+        int max = Integer.MIN_VALUE;
+        for (int value : data) {
+            if (value > max) {
+                max = value;
+            }
+        }
+        System.out.println("Max value found:" + max);
+         
         ForkJoinPool pool = new ForkJoinPool();
+        
+        RandomArrayAction action = new RandomArrayAction(data, 0, data.length-1, data.length/16);
+        pool.invoke(action);
+        
         FindMaxTask task = new FindMaxTask(data, 0, data.length-1, data.length/16);
         Integer result = pool.invoke(task);
         System.out.println("Max value found:" + result);

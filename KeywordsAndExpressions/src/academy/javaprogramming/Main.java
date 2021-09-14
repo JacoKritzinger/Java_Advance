8958f15374f29907224210dfb77a4eacd71aac21
jaco Kritzinger <jacokritzinger05@gmail.com>
2021-09-14 17:05:56 +0200

    14/09/2021

---
+++ KeywordsAndExpressions/src/academy/javaprogramming/Main.java
@@ -0,0 +1,33 @@
+package academy.javaprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+//        // a mile is equal to 1.609344 kilometers
+//        double kilometers = (100 * 1.609344);
+//        int highScore = 50;
+//        if(highScore == 50) {
+//            System.out.println("this is an expression");
+
+
+//        int score = 100;
+//        if (score > 99) {
+//            System.out.println("you got the high score!");
+//            score = 0;
+//        }
+
+        int myVariable = 50;
+        myVariable++;
+        myVariable--;
+        System.out.println("this is a test");
+
+        System.out.println("This is" + "another" + "still more.");
+
+
+
+
+
+    }
+}

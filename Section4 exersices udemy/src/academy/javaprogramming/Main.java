b28c347fae107581283d527de868f6cdce66e560
jaco Kritzinger <jacokritzinger05@gmail.com>
2021-09-16 10:14:17 +0200

    16/09/2021

--- Section4 exersices udemy/src/academy/javaprogramming/Main.java
+++ Section4 exersices udemy/src/academy/javaprogramming/Main.java
@@ -2,13 +2,45 @@ package academy.javaprogramming;
 
 public class Main {
 
-    public static void printMegaBytesAndKiloBytes(int kiloBytes) {
-        if (kiloBytes > 0) {
-            double remainder = kiloBytes % 1024;
-            System.out.println(kiloBytes + " KB = " + ((kiloBytes / 1024) - remainder) + " MB " + (remainder * 1024) + " KB");
+
+//    1.
+//        public static long toMilesPerHour(double kilometersPerHour){
+//            if (kilometersPerHour < 0){
+//                return -1;
+//            }
+//            return Math.round((kilometersPerHour / 1.609));
+//        }
+//
+//        public static void printConversion(double kilometersPerHour){
+//            if (kilometersPerHour < 0){
+//                System.out.println("Invalid Value");;
+//            }
+//            else{
+//                System.out.println(kilometersPerHour + " km/h = " + toMilesPerHour(kilometersPerHour) + " mi/h");
+//            }
+//        }
+//    }
+//2.
+//    public static void printMegaBytesAndKiloBytes(int kiloBytes) {
+//        if (kiloBytes > 0) {
+//            double remainder = kiloBytes % 1024;
+//            System.out.println(kiloBytes + " KB = " + ((kiloBytes / 1024) - remainder) + " MB " + (remainder * 1024) + " KB");
+//        }
+//        else{
+//            System.out.println("Invalid Value");
+//        }
+//    }
+//}
+
+//    3.
+
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
+        if (hourOfDay < 0 || hourOfDay > 23){
+            return false;
         }
-        else{
-            System.out.println("Invalid Value");
+        else if ((barking) && (hourOfDay < 8 || hourOfDay > 22)){
+            return true;
         }
+        return false;
     }
 }

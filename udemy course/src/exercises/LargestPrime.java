f6d459a614b5293b66bdf34fd887a3356d4065b8
jaco Kritzinger <jacokritzinger05@gmail.com>
2021-09-29 13:52:44 +0200

    28/09/2021

---
+++ udemy course/src/exercises/LargestPrime.java
@@ -0,0 +1,16 @@
+package exercises;
+
+public class LargestPrime {
+    public static int getLargestPrime(int number) {
+        if(number <= 1) return -1;
+
+        for (int primeFactor = 2;primeFactor < number;primeFactor++) {
+            if (number%primeFactor ==0) {
+                number /= primeFactor;
+                primeFactor--;
+            }
+        }
+        return number;
+
+    }
+}

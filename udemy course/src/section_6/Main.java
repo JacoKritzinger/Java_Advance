13de0a2f267c90315a309f9a46efb7dd8a2b583c
jaco Kritzinger <jacokritzinger05@gmail.com>
2021-10-04 09:41:38 +0200

    04/10/2021

--- udemy course/src/section_6/Main.java
+++ udemy course/src/section_6/Main.java
@@ -8,20 +8,34 @@ public class Main {
 //        System.out.println("Model is " + porsche.getModel());
 //    }
 
-        BankAccount bobsAccount = new BankAccount("12345", 0.00, "Bob brown", "BoB@gmail.com2", "0821234567");
+//        vid 78
 
-        System.out.println(bobsAccount.getNumber());
-        System.out.println(bobsAccount.getBalance());
+//        BankAccount bobsAccount = new BankAccount("12345", 0.00, "Bob brown", "BoB@gmail.com2", "0821234567");
+//
+//        System.out.println(bobsAccount.getNumber());
+//        System.out.println(bobsAccount.getBalance());
+//
+//        bobsAccount.withdraw(100);
+//
+//        bobsAccount.deposit(50);
+//        bobsAccount.withdraw(100);
+//
+//        bobsAccount.deposit(51);
+//        bobsAccount.withdraw(100);
+//
+//        BankAccount timAccount = new BankAccount("Tim", " tim@gmail.com", "12345");
+//        System.out.println(timAccount.getNumber() + " name " + timAccount.getCustomerName());
 
-        bobsAccount.withdraw(100);
+//        vid 80
+//
+        VipPerson person1 = new VipPerson();
+        System.out.println(person1.getName());
 
-        bobsAccount.deposit(50);
-        bobsAccount.withdraw(100);
+        VipPerson person2 = new VipPerson("Bob", 25000.00);
+        System.out.println(person2.getName());
 
-        bobsAccount.deposit(51);
-        bobsAccount.withdraw(100);
+        VipPerson person3 = new VipPerson("Tom", 100.00, "Tom@email.com");
+        System.out.println(person3.getName());
 
-        BankAccount timAccount = new BankAccount("Tim", " tim@gmail.com", "12345");
-        System.out.println(timAccount.getNumber() + " name " + timAccount.getCustomerName());
     }
 }

5128120b13cde0700006ee10cd6fb651fffb9489
jaco Kritzinger <jacokritzinger05@gmail.com>
2021-10-11 13:29:22 +0200

    11/10/2021

--- udemy course/src/section_6/Animal.java
+++ udemy course/src/section_6/Animal.java
@@ -19,7 +19,8 @@ public class Animal {
         System.out.println("Animal.eat() called");
 
     }
-    public void move(){
+    public void move(int speed) {
+        System.out.println("Animal.move() called.  Animal was moving at " + speed + " Km/h");
 
     }
 

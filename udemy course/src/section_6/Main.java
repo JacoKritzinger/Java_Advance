21f9d234f5223080e67dcd6291127f2bbfd4641d
jaco Kritzinger <jacokritzinger05@gmail.com>
2021-10-12 23:36:59 +0200

    12/10/2021

--- udemy course/src/section_6/Main.java
+++ udemy course/src/section_6/Main.java
@@ -39,6 +39,13 @@ public class Main {
 
 //        vid 81
 
+//        Animal animal = new Animal("Animal", 1, 1, 5, 5);
+//
+//        Dog dog = new Dog("Yorkie", 8, 20, 2, 4, 1, 20, "long silkey");
+//        dog.eat();
+//        dog.walk();
+//        dog.run();
+
         Animal animal = new Animal("Animal", 1, 1, 5, 5);
 
         Dog dog = new Dog("Yorkie", 8, 20, 2, 4, 1, 20, "long silkey");
